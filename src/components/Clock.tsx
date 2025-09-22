a9bc08a9023de8d8772770b01f730314fb33172a
sekforde <nigelwatson@me.com>
2025-09-22 17:14:03 +0100

    clock

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,9 +1,21 @@
 'use client'
 
+import { useState, useEffect } from 'react'
+
 export function Clock() {
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date())
+        }, 5000) // Update every 5 seconds
+
+        return () => clearInterval(interval)
+    }, [])
+
     return (
         <div className="w-12 pt-2" id="clock">
-            {new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+            {time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
         </div>
     )
 }
